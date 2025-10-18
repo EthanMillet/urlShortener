22c8b03196871ea8d192b53df9f1b4ac89679989
Ethan Millet <mirreto@Ethans-MacBook-Pro.local>
2025-10-18 14:12:27 -0600

    Fix models/index.js to handle production environment without config.json

--- url-api/models/index.js
+++ url-api/models/index.js
@@ -6,14 +6,22 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (env === 'production') {
+  // For production with JawsDB
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    dialect: 'mysql'
+  });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // For development and test environments
+  const config = require(__dirname + '/../config/config.json')[env];
+  if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  } else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
 }
 
 fs
